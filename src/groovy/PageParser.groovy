80b56ff8383a6c1957b651fa6e18fb14b2ac23ee
GKhotyan <georgiy.hotiyan@masterdata.ru>
2016-09-05 19:55:27 +0300

    #AL-001 sensless commit

--- src/groovy/PageParser.groovy
+++ src/groovy/PageParser.groovy
@@ -3,7 +3,7 @@ class PageParser {
     public String getVersion(String url) {
 //        @Grab('org.jsoup:jsoup:1.7.1')
 
-        def doc = org.jsoup.Jsoup.connect("https://itunes.apple.com/ru/app/uber/id368677368")
+        def doc = org.jsoup.Jsoup.connect(url)
                 .userAgent("Mozilla")
                 .get()
 
